5ef756cce3791ffd8bb25c2e000145f960ef15bc
Antonin <antonin.prouteau@spikeelabs.fr>
2021-05-17 12:21:25 +0200

    fix doctolib ko error

--- doctolib-covid.py
+++ doctolib-covid.py
@@ -1,6 +1,7 @@
 import datetime
 import requests
 import time
+import json
 
 with open('centers-url.txt') as centers_url_txt:
     centers_urls = centers_url_txt.readlines()
@@ -12,62 +13,65 @@ try:
     print()
     while True :
         for center_url in centers_urls:
-            center = center_url.split("/")[5]
+            try :
+                center = center_url.split("/")[5]
+                raw_data = requests.get("https://www.doctolib.fr/booking/{center}.json")
+                json_data = raw_data.json()
+                data = json_data["data"]
 
-            data = requests.get(
-                f"https://www.doctolib.fr/booking/{center}.json").json()["data"]
-
-            visit_motives = [visit_motive for visit_motive in data["visit_motives"]
-                            if visit_motive["name"].startswith("1re injection") and
-                            "AstraZeneca" not in visit_motive["name"]]
-            if not visit_motives:
-                continue
-
-            places = [place for place in data["places"]]
-            if not places:
-                continue
-
-            for place in places:
-
-                start_date = datetime.datetime.today().date().isoformat()
-                visit_motive_ids = visit_motives[0]["id"]
-                practice_ids = place["practice_ids"][0]
-                place_name = place["formal_name"]
-                place_address = place["full_address"]
-
-                agendas = [agenda for agenda in data["agendas"]
-                        if agenda["practice_id"] == practice_ids and
-                        not agenda["booking_disabled"] and
-                        visit_motive_ids in agenda["visit_motive_ids"]]
-                if not agendas:
+                visit_motives = [visit_motive for visit_motive in data["visit_motives"]
+                                if visit_motive["name"].startswith("1re injection") and
+                                "AstraZeneca" not in visit_motive["name"]]
+                if not visit_motives:
                     continue
 
-                agenda_ids = "-".join([str(agenda["id"]) for agenda in agendas])
+                places = [place for place in data["places"]]
+                if not places:
+                    continue
 
-                # print(visit_motive_ids)
-                # print(practice_ids)
-                # print(agenda_ids)
+                for place in places:
 
-                response = requests.get(
-                    "https://www.doctolib.fr/availabilities.json",
-                    params={
-                        "start_date": start_date,
-                        "visit_motive_ids": visit_motive_ids,
-                        "agenda_ids": agenda_ids,
-                        "practice_ids": practice_ids,
-                        "insurance_sector": "public",
-                        "destroy_temporary": "true",
-                        "limit": 2
-                    },
-                )
-                response.raise_for_status()
-                nb_availabilities = response.json()["total"]
+                    start_date = datetime.datetime.today().date().isoformat()
+                    visit_motive_ids = visit_motives[0]["id"]
+                    practice_ids = place["practice_ids"][0]
+                    place_name = place["formal_name"]
+                    place_address = place["full_address"]
 
-                result = datetime.datetime.now().strftime("%H:%M:%S") + " " + str(nb_availabilities) + \
-                    " appointments are available : " + center_url + \
-                    "?pid=practice-"+str(practice_ids)
-                if nb_availabilities > 0:
-                    print(result)
-        time.sleep(3)
+                    agendas = [agenda for agenda in data["agendas"]
+                            if agenda["practice_id"] == practice_ids and
+                            not agenda["booking_disabled"] and
+                            visit_motive_ids in agenda["visit_motive_ids"]]
+                    if not agendas:
+                        continue
+
+                    agenda_ids = "-".join([str(agenda["id"]) for agenda in agendas])
+
+                    # print(visit_motive_ids)
+                    # print(practice_ids)
+                    # print(agenda_ids)
+
+                    response = requests.get(
+                        "https://www.doctolib.fr/availabilities.json",
+                        params={
+                            "start_date": start_date,
+                            "visit_motive_ids": visit_motive_ids,
+                            "agenda_ids": agenda_ids,
+                            "practice_ids": practice_ids,
+                            "insurance_sector": "public",
+                            "destroy_temporary": "true",
+                            "limit": 2
+                        },
+                    )
+                    response.raise_for_status()
+                    nb_availabilities = response.json()["total"]
+
+                    result = datetime.datetime.now().strftime("%H:%M:%S") + " " + str(nb_availabilities) + \
+                        " appointments are available : " + center_url + \
+                        "?pid=practice-"+str(practice_ids)
+                    if nb_availabilities > 0:
+                        print(result)
+            except json.decoder.JSONDecodeError:
+                print("Doctolib might be ko")
+        time.sleep(5)
 except KeyboardInterrupt:
      print("Mischief managed.")
